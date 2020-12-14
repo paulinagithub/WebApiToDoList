e5820af018f23226c1508633610b9e0f67610771
unknown <paulinex3910@gmail.com>
2020-12-14 20:11:41 +0100

    Add summary to interfaces

--- WebApiToDo/Repositories/Interfaces/IToDoRepository.cs
+++ WebApiToDo/Repositories/Interfaces/IToDoRepository.cs
@@ -12,30 +12,35 @@ namespace WebApiToDo.Repositories.Interfaces
         /// Return list with all items
         /// </summary>
         Task<List<ToDoModel>> GetAllItemsAsync();
+
         /// <summary>
         /// Add item to DB
         /// </summary>
-        /// <param name="toDo"></param>
+        /// <param name="toDo"></param>      
         Task AddItemAsync(ToDoModel toDo);
+
         /// <summary>
         /// Remove item from DB
         /// </summary>
-        /// <param name="toDo"></param>
+        /// <param name="toDo"></param>      
         Task DeleteItemAsync(ToDoModel toDo);
+
         /// <summary>
         /// Check if item is in DB
         /// </summary>
-        /// <param name="id"></param>
+        /// <param name="id"></param>       
         Task<ToDoModel> FindItemByIDAsync(int id);
+
         /// <summary>
         /// Update item in DB
         /// </summary>
-        /// <param name="todo"></param>
+        /// <param name="todo"></param>       
         Task UpdateToDoAsync(ToDoModel todo);
+
         /// <summary>
         /// Return list of all elements filter by given parameters 
         /// </summary>
-        /// <param name="isCompleted"></param>
+        /// <param name="isCompleted"></param>       
         Task<List<ToDoModel>> GetAllItemsFilterAsync(int isCompleted);
     }
 }
--- WebApiToDo/Services/Interfaces/IToDoService.cs
+++ WebApiToDo/Services/Interfaces/IToDoService.cs
@@ -8,14 +8,34 @@ namespace WebApiToDo.Services.Interfaces
 {
     public interface IToDoService
     {
+        /// <summary>
+        /// Add new item
+        /// </summary>
+        /// <param name="todoModel"></param>
         Task AddItemAsync(ToDoDTO todoModel);
 
+        /// <summary>
+        /// Delete item by id
+        /// </summary>
+        /// <param name="id"></param>
         Task<bool> DeleteItemAsync(int id);
 
+        /// <summary>
+        /// Get all item from DB
+        /// </summary>
         Task<List<ToDoDTO>> GetAllItemsAsync();
 
+        /// <summary>
+        /// Get all item with bool filter isCompleted
+        /// </summary>
+        /// <param name="isCompleted"></param>
         Task<List<ToDoDTO>> GetAllItemsFilterAsync(bool isCompleted);
 
+        /// <summary>
+        /// Update item with new model by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="todo"></param>
         Task UpdateItemAsync(int id, ToDoDTO todo);
     }
 }
